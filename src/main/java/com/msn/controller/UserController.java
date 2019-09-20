9451b64a1d94e4dba7086b759b2fe62724be4e50
Md. Shafi Nawaz <md.shafi-nawaz@capgemini.com>
2019-09-20 18:06:30 +0530

    Initial commit

---
+++ src/main/java/com/msn/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.msn.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.msn.SpringBootLoggingExampleApplication;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	Logger log = LoggerFactory.getLogger(SpringBootLoggingExampleApplication.class);
+
+	@GetMapping("/{name}")
+	public String greeting(@PathVariable String name) {
+		log.debug("Request {}", name);
+		if (name.equalsIgnoreCase("test")) {
+			throw new RuntimeException("Opps Exception raised....");
+		}
+		String response = "Hi " + name + " Welcome to Java Techie";
+		log.debug("Response {}", response);
+		return response;
+	}
+}
